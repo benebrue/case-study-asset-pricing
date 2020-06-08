7f2d4243eb3e3acb88fbff9dc76ed1865b579e18
= <benediktbrueckner@web.de>
2020-06-08 22:59:20 +0200

    Add computation of the quantiles and correct computation of dates/available_returns

--- exercise1.R
+++ exercise1.R
@@ -6,6 +6,8 @@ library(lubridate)
 library(dplyr)
 # read file with stock data
 stockdata<-fread("./data/stock_data.csv")
+#!!!for development purposes only: reduce size of the dataset
+stockdata<-stockdata[1:10000,]
 stockdata
 
 # implement a monthly momentum portfolio (daily version described in the paper would require information on daily 
@@ -30,7 +32,8 @@ stockdata[,log_return:=log(1+return)]
 stockdata
 
 # calculate start of the formation period for each date
-ranking_start<-stockdata$date %m-% months(12) # subtract twelve months
+ranking_start<-stockdata$date %m-% months(11) # subtract eleven months because the return stored as that of the 
+# point in time 11 months ago is the one realized starting 12 months ago
 day(ranking_start)<-days_in_month(ranking_start) # set day of the date = last day of the month
 stockdata$ranking_start<-ranking_start
 
@@ -40,29 +43,47 @@ day(ranking_end)<-days_in_month(ranking_end) # set day of the date = last day of
 stockdata$ranking_end<-ranking_end
 stockdata
 
+# for testing the start/end dates
+# sdate<-stockdata[,.(ranking_start)][11]
+# edate<-stockdata[,.(ranking_end)][11]
+# stockdata[permno == 10001,] %>% filter(date >= sdate & date <= edate)
+
 # calculate cumulative log returns for each row
-# define function 
-csum = function(permno_input, startdate, enddate){ 
-    sum((stockdata %>% filter(permno == permno_input & date >= startdate & date <= enddate))$value)
+# define function
+csum = function(permno_input, startdate, enddate){
+    sum((stockdata[permno == permno_input,] %>% filter(date >= startdate & date <= enddate))$log_return)
 }
 
 csum = Vectorize(csum)
 
 #execute calculation
-stockdata %>% mutate(cum_log_return = csum(permno, ranking_start, ranking_end))
+stockdata$cum_log_return<-mapply(csum, stockdata$permno, stockdata$ranking_start, stockdata$ranking_end)
 stockdata
 
 # calculate number of stock returns available for the past 11 months 
 fcount = function(permno_input, startdate, enddate){
-    length((stockdata %>% filter(permno == permno_input & date >= startdate & date <= enddate)))
+    nrow((stockdata %>% filter(permno == permno_input & date >= startdate & date <= enddate)))
 }
 
 fcount = Vectorize(fcount)
 
 #execute counting
-stockdata %>% mutate(available_returns = fcount(permno, ranking_start, ranking_end))
+stockdata$available_returns<-mapply(fcount, stockdata$permno, stockdata$ranking_start, stockdata$ranking_end)
 stockdata
 
-# TODO: next step would be to group data by date, determine the shares that have available_returns>=8 for each date
-# as specified in the paper and then divide these into ten deciles before extracting the winners (top decile) and
-# losers (bottom decile)
+# remove rows which have available_returns < 8
+stockdata<-stockdata[available_returns >= 8,]
+stockdata
+
+# cut returns for each date into deciles to determine winner/loser portfolios
+stockdata[,bin:=cut(cum_log_return,
+                    quantile(cum_log_return,probs=seq(from=0,to=1,by=1/10),na.rm =T),
+                    include.lowest=TRUE, 
+                    labels=FALSE),
+          by=date]
+stockdata
+
+
+
+# TODO: next step would be to extract the winners (top decile --> long) and losers (bottom decile --> short) for each 
+# date and compute their average return
