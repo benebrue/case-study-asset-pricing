f5481116c338e87a1fecdc0e385901990dc00d43
= <benediktbrueckner@web.de>
2020-06-10 18:26:10 +0200

    Finalize computation of WML portfolio returns

--- exercise1.R
+++ exercise1.R
@@ -8,6 +8,7 @@ library(dplyr)
 stockdata<-fread("./data/stock_data.csv")
 #!!!for development purposes only: reduce size of the dataset
 stockdata<-stockdata[1:10000,]
+stockdata<-stockdata[complete.cases(stockdata), ]
 stockdata
 
 # implement a monthly momentum portfolio (daily version described in the paper would require information on daily 
@@ -83,7 +84,46 @@ stockdata[,bin:=cut(cum_log_return,
           by=date]
 stockdata
 
+# get data of the stocks in the top decile
+top<-stockdata[bin == 10]
+#calculate sum of the market capitalization for each date
+top$tcapsum<-ave(top$tcap, top$date, FUN=sum)
+top
+# get data of the stocks in the bottom decile
+bottom<-stockdata[bin == 1]
+#calculate sum of the market capitalization for each date
+bottom$tcapsum<-ave(bottom$tcap, bottom$date, FUN=sum)
+bottom
+
+# group by date and calculate value-weighted return
+top<-top %>% group_by(date) %>%
+    summarise(vw_return = sum(return * tcap / tcapsum))
+top
+
+# convert back from a tibble to a data.table
+setDT(top)
+top
+
+bottom<-bottom %>% group_by(date) %>%
+    summarise(vw_return = sum(return * tcap / tcapsum))
+bottom
+# convert back from a tibble to a data.table
+setDT(bottom)
+bottom
+
+portfolios<-merge(bottom, top, by.x="date", by.y="date")
+colnames(portfolios)<-c("date", "return_bottom", "return_top")
+portfolios
+
+portfolios[,return_wml:=return_top-return_bottom]
+portfolios
+
+# at this the step "portfolios" contains the value-weighted returns of the top 10% portfolio, the value-weighted
+# returns of the bottom 10% portfolio and the returns of the WML strategy (long top, short bottom)
 
 
-# TODO: next step would be to extract the winners (top decile --> long) and losers (bottom decile --> short) for each 
-# date and compute their average return
+# next step is to calculate the weights necessary for balancing WML portfolio and risk-free asset
+# to calculate the weights, we need conditional expected return and conditional variance for the coming month
+
+# proxy for conditional expected return: fitted regression of WML returns on interaction between bear market indicator
+# and market variance over the preceding six months
